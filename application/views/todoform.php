dc5e3d340422083c8156a2c1ada508761810bbfd
Terry Morgan <terry.morgan76@gmail.com>
2011-04-23 21:09:14 +0100

    Added form and list views

---
+++ application/views/todoform.php
@@ -0,0 +1,11 @@
+<form action="/" method="post">
+  <input type="hidden" name="_submit_check" value="1" />
+  <fieldset>
+    <div class="input text">
+      <input type="text" name="desc" id="desc" value="" />
+    </div>
+    <div class="buttons submit">
+      <input type="submit" name="submit" id="submit" value="Save" />
+    </div>
+  </fieldset>
+</form>
--- application/views/todolist.php
+++ application/views/todolist.php
@@ -1,10 +1,29 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
-<h1>Todo list</h1>
+<h1>My ToDos</h1>
 
-<ul>
-  <li><a href="#"></a></li>
-  <li><a href="#"></a></li>
-  <li><a href="#"></a></li>
-  <li><a href="#"></a></li>
-</ul>
+<?php echo $todoForm ?>
+
+<table>
+  <thead>
+    <tr>
+      <th>Status</th>
+      <th>Due date</th>
+      <th>Desc</th>
+      <th>Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($todos as $id => $todo): ?>
+    <tr>
+      <td><a href='/todos/complete/<?php echo $id ?>'><?php echo $todo->complete ? 'Complete' : 'Incomplete'; ?></a></td>
+      <td><?php echo $todo->dueDate; ?></td>
+      <td><?php echo $todo->desc; ?></td>
+      <td>
+        <a href="/todos/edit/<?php echo $id; ?>">Edit</a>
+        <a href="/todos/delete/<?php echo $id; ?>">Delete</a>
+      </td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
