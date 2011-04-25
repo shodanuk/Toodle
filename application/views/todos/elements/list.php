0c058f02cfbf6902f151ce55aeaafb80781a3f2e
Terry Morgan <terry.morgan76@gmail.com>
2011-04-25 18:43:59 +0100

    Some styling for todo list

--- application/views/todos/elements/list.php
+++ application/views/todos/elements/list.php
@@ -1,29 +1,33 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
-<?php if (count($todos) > 0): ?>
-  <table>
-    <thead>
-      <tr>
-        <th>Status</th>
-        <th>Due date</th>
-        <th>Desc</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach($todos as $todo): ?>
-      <tr class="<?php echo $todo->complete ? 'Complete' : 'Incomplete'; ?>">
-        <td><a href='/todos/complete/<?php echo $todo->id ?>'><?php echo $todo->complete ? 'Complete' : 'Incomplete'; ?></a></td>
-        <td><?php echo $todo->due_date; ?></td>
-        <td><?php echo $todo->description; ?></td>
-        <td>
-          <a href="/todos/index/<?php echo $todo->id; ?>">Edit</a>
-          <a href="/todos/delete/<?php echo $todo->id; ?>">Delete</a>
-        </td>
-      </tr>
-      <?php endforeach; ?>
-    </tbody>
-  </table>
-<?php else: ?>
-  <p>Wow, no ToDo items?! Take the day off!!</p>
-<?php endif; ?>
+<div class="todolist-wrapper">
+  <?php if (count($todos) > 0): ?>
+    <table class='todo-list' cellspacing="0">
+      <thead>
+        <tr>
+          <th>Status</th>
+          <th>Due date</th>
+          <th>Desc</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach($todos as $todo): ?>
+        <?php $status = $todo->complete ? 'Complete' : 'Incomplete'; ?>
+
+        <tr class="<?php echo $status; ?>">
+          <td><a href='/todos/complete/<?php echo $todo->id ?>' class="ir <?php echo $status; ?>"><?php echo $status; ?></a></td>
+          <td><?php echo $todo->due_date; ?></td>
+          <td><?php echo $todo->description; ?></td>
+          <td>
+            <a href="/todos/index/<?php echo $todo->id; ?>">Edit</a>
+            <a href="/todos/delete/<?php echo $todo->id; ?>">Delete</a>
+          </td>
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  <?php else: ?>
+    <p>Wow, no ToDo items?! Take the day off!!</p>
+  <?php endif; ?>
+</div>
