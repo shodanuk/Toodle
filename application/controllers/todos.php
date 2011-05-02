69054e2cbe7559d3dd910e9d092042f9d65aa821
Terry Morgan <terry.morgan76@gmail.com>
2011-05-02 22:59:49 +0100

    - Code tidying + commenting. - Various updates to saving and editing functionality - Merged due date entry into description field - Added filtering todos by user - Added order todos by due date

--- application/controllers/todos.php
+++ application/controllers/todos.php
@@ -1,143 +1,193 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
+// === TextMate error handling ===
+@include_once '/Applications/TextMate.app/Contents/SharedSupport/Bundles/PHP.tmbundle/Support/textmate.php';
 
+/**
+ * Todos_Controller
+ *
+ * @package     Toodle
+ * @author      Terry Morgan <terry.morgan@marmaladeontoast.co.uk>
+ * @copyright   (c) 2011 Terry Morgan
+ */
 class Todos_Controller extends Controller {
 
-  public function __construct() {
+  public function __construct()
+  {
     parent::__construct();
     $this->session = Session::instance();
 
-    $auth = new Auth();
-    if (!$auth->logged_in()){
-      $this->session->set("requested_url","/".url::current()); // this will redirect from the login page back to this page
+    $auth = new Auth;
+    if ($auth->logged_in() || $auth->auto_login() == TRUE)
+    {
+      $this->user = $auth->get_user();
+    }
+    else
+    {
+      $this->session->set('requested_url', '/'.url::current());
       url::redirect('/users/login');
-    }else{
-      $this->user = $auth->get_user(); //now you have access to user information stored in the database
     }
   }
 
-  public function index($id = false) {
-    $view = new View('todos/index');
+  public function index($id = false)
+  {
+    $view             = new View('todos/index');
+    $view->header     = new View('layout/header');
+    $view->footer     = new View('layout/footer');
+    $view->todo_form  = new View('todos/elements/form');
+    $view->todo_list  = new View('todos/elements/list');
 
-    $view->header = new View('layout/header');
-    $view->footer = new View('layout/footer');
-
-    $view->todoForm = new View('todos/elements/form');
-    $view->todoList = new View('todos/elements/list');
-
-    $view->header->pageTitle  = "Your ToDos";
-    $view->header->isLoggedin = true;
-    $view->header->username   = $this->user->username;
+    $view->header->page_title   = 'Your ToDos';
+    $view->header->is_logged_in = true;
+    $view->header->username     = $this->user->username;
+    $view->header->body_class   = 'index';
 
     $view->message_type  = $this->session->get('message_type') ? $this->session->get_once('message_type') : false;
     $view->message       = $this->session->get('message') ? $this->session->get_once('message') : false;
 
-    $view->todoForm->errors = null;
+    $view->todo_form->errors = null;
 
-    // setup and initialize your form field names
+    // Setup and initialize your form field names
     $form = array(
       'description' => '',
-      'due_date'    => '',
       'id'          => '',
     );
 
-    //  copy the form as errors, so the errors will be stored with keys corresponding to the form field names
+    // Copy the form as errors, so the errors will be stored with keys corresponding to the form field names
     $errors = $form;
 
-    if( $_POST ) {
+    if ($_POST)
+    {
       $post = new Validation($_POST);
 
       $post->pre_filter('trim', TRUE);
       $post->add_rules('description', 'required');
-      $post->add_rules('due_date', 'required');
 
-      if( $post->validate() ) {
-        // Attempt to save the todo item. If an id has been passed thru, we're editing
-        // an existing item, otherwise we create a new item
-        $saveStatus = $this->input->post('id') ? $this->_save($this->input->post('id')) : $this->_save();
+      if ($post->validate())
+      {
+        // Process the description and attempt to extract a due date and / or time,
+        // if they've been entered.
 
-        if( $saveStatus ) {
-          $view->message_type = "good";
-          $view->message      = "ToDo item saved.";
+        $id = $this->input->post('id');
+        $todo = new Todo_Model($id);
 
-          $view->todoForm->description  = "";
-          $view->todoForm->due_date     = "";
-          $view->todoForm->id           = "";
-        } else {
-          $view->message_type = "error";
-          $view->message      = "Save failed. Please try again.";
+        if ( ! $id)
+        {
+          $todo->date_added = Date('Y-m-d H:i:s');
         }
-      } else {
-        // repopulate the form fields
-        $view->todoForm->id           = $this->input->post('id');
-        $view->todoForm->description  = $this->input->post('description');
-        $view->todoForm->due_date     = $this->input->post('due_date');
 
-        // populate the error fields, if any
-        $view->todoForm->errors = arr::overwrite($errors, $post->errors('form_errors'));
-        $view->message_type     = "error";
-        $view->message          = "Please correct the errors below and try again.";
+        $todo->complete = $this->input->post('complete');
+        $todo->user_id  = $this->user->id;
+
+        // get_due_date will return an array of due date and description if a due date has
+        // been enter and a string containing the description, if not.
+        $desc_parts = todo::get_due_date($this->input->post('description'));
+
+        if (is_array($desc_parts))
+        {
+          $todo->description  = trim($desc_parts['description']);
+
+          // A due date and a description have been entered, validate the due date
+          $due_date = trim($desc_parts['due_date']);
+
+          // Attempt to extract a valid date and time from the due date
+          if ($mysql_timestamp = todo::convert_due_date($due_date))
+          {
+            $todo->due_date = $mysql_timestamp;
+          }
+        }
+        else
+        {
+          $todo->description  = trim($desc_parts);
+        }
+
+        if ($todo->save())
+        {
+          $view->message_type = 'good';
+          $view->message      = 'ToDo item saved.';
+
+          $view->todo_form->description  = "";
+          $view->todo_form->id           = "";
+        }
+        else
+        {
+          $view->message_type = 'error';
+          $view->message      = 'Save failed. Please try again.';
+        }
       }
+      else
+      {
+        // Repopulate the form fields
+        $view->todo_form->id           = $this->input->post('id');
+        $view->todo_form->description  = $this->input->post('description');
 
-    } else if( $id ) {
+        // Populate the error fields, if any
+        $view->todo_form->errors  = arr::overwrite($errors, $post->errors('form_errors'));
+        $view->message_type       = 'error';
+        $view->message            = 'Please correct the errors below and try again.';
+      }
+    }
+    elseif ($id)
+    {
       // Attempt to load the requested todo item
-      $editTodo = new Todo_Model($id);
+      $edit_todo = new Todo_Model($id);
 
-      if ( $editTodo->loaded ) {
-        $view->todoForm->id           = $editTodo->id;
-        $view->todoForm->description  = $editTodo->description;
-        $view->todoForm->due_date     = $editTodo->due_date;
-      } else {
-        $view->message_type = "error";
+      if ($edit_todo->loaded)
+      {
+        $view->todo_form->id           = $edit_todo->id;
+        $view->todo_form->description = $edit_todo->due_date
+                                          ? todo::display_due_date($edit_todo->due_date)." | ".$edit_todo->description
+                                          : $edit_todo->description;
+      }
+      else
+      {
+        $view->message_type = 'error';
         $view->message      = 'ToDo item not found.';
       }
-
     }
 
-    $view->todoList->todos = ORM::factory('todo')->find_all();
+    $per_page = 10;
+    $offset   = 0;
+
+    $view->todo_list->todos = ORM::factory('todo')
+                                ->where('user_id', $this->user->id)
+                                ->orderby('due_date', 'ASC')
+                                ->find_all($per_page, $offset);
     $view->render(TRUE);
   }
 
-  public function delete($id) {
+  public function delete($id)
+  {
     ORM::factory('todo')->delete($id);
     $this->session->set_flash(array('message_type'=>'good', 'message'=>'ToDo item deleted.'));
     url::redirect('/');
   }
 
-  public function complete($id = false) {
-    if ( $id ) {
+  public function complete($id = false)
+  {
+    if ($id)
+    {
       $todo = new Todo_Model($id);
-      if ( $todo->loaded ) {
+      if ($todo->loaded) {
         $todo->complete = $todo->complete ? 0 : 1;
-        $todo->save();
 
-        if ( $todo->saved ) {
-          if ( $todo->complete ) {
-            $this->session->set_flash(array('message_type'=>'good', 'message'=>'ToDo item completed.'));
-          } else {
-            $this->session->set_flash(array('message_type'=>'good', 'message'=>'ToDo item uncompleted.'));
+        if ($todo->save())
+        {
+          if ($todo->complete)
+          {
+            $flash_message = array('message_type'=>'good', 'message'=>'ToDo item completed.');
           }
-        } else {
-          $this->session->set_flash(array('message_type'=>'error', 'message'=>'ToDo item could not be saved.'));
+          else
+          {
+            $flash_message = array('message_type'=>'good', 'message'=>'ToDo item uncompleted.');
+          }
         }
+        else
+        {
+          $flash_message = array('message_type'=>'error', 'message'=>'ToDo item could not be saved.');
+        }
+        $this->session->set_flash($flash_message);
       }
     }
-
     url::redirect('/');
-  }
-
-  private function _save($id = false) {
-    $todo = $id ? new Todo_Model($id) : new Todo_Model();
-    $todo->description = $this->input->post('description');
-    $todo->due_date = $this->input->post('due_date');
-    $todo->complete = $this->input->post('complete');
-
-    if( $id ) {
-      $todo->date_modified = Date('Y-m-d H:i:s');
-    } else {
-      $todo->date_added = Date('Y-m-d H:i:s');
-    }
-
-    $todo->save();
-    return $todo->saved;
   }
 } // End Todos Controller
