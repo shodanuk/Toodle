cc41aaaa35a9ffa3ca13e908c6d7eb1d46ffab7f
Terry Morgan <terry.morgan76@gmail.com>
2011-05-02 22:53:21 +0100

    Updated db SQL

--- toodle.sql
+++ toodle.sql
@@ -2,9 +2,9 @@
 # Version 2492
 # http://code.google.com/p/sequel-pro
 #
-# Host: 127.0.0.1 (MySQL 5.1.42)
+# Host: localhost (MySQL 5.1.42)
 # Database: toodle
-# Generation Time: 2011-04-25 14:39:49 +0100
+# Generation Time: 2011-05-02 22:52:38 +0100
 # ************************************************************
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -55,6 +55,14 @@ CREATE TABLE `roles_users` (
   CONSTRAINT `roles_users_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `roles_users` WRITE;
+/*!40000 ALTER TABLE `roles_users` DISABLE KEYS */;
+INSERT INTO `roles_users` (`user_id`,`role_id`)
+VALUES
+	(1,1);
+
+/*!40000 ALTER TABLE `roles_users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table todos
@@ -65,30 +73,31 @@ DROP TABLE IF EXISTS `todos`;
 CREATE TABLE `todos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `description` text NOT NULL,
-  `due_date` datetime NOT NULL,
+  `due_date` timestamp NULL DEFAULT NULL,
   `complete` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `date_added` datetime NOT NULL,
-  `date_modified` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+  `date_added` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `due_date` (`due_date`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `todos` WRITE;
 /*!40000 ALTER TABLE `todos` DISABLE KEYS */;
-INSERT INTO `todos` (`id`,`description`,`due_date`,`complete`,`date_added`,`date_modified`)
+INSERT INTO `todos` (`id`,`description`,`due_date`,`complete`,`date_added`,`date_modified`,`user_id`)
 VALUES
-	(2,'Invade China - again!','2011-04-24 07:30:00',0,'2011-04-21 22:47:48','2011-04-25 11:40:38'),
-	(3,'Start astronaut training at NASA.','2011-04-24 07:30:00',0,'2011-04-21 22:47:48','0000-00-00 00:00:00'),
-	(4,'1Solve the riddle of the meaning of life. Post on Reddit. Sit back and relax as upvotes flood in.','2011-04-25 12:10:00',0,'2011-04-21 22:47:48','2011-04-24 18:06:31'),
-	(5,'Find all missing socks','2011-04-25 13:08:00',0,'2011-04-21 22:47:48','2011-04-24 21:59:33'),
-	(6,'33Win Premier League with a team consisting only of Ali Dia clones.','2011-04-27 10:40:00',0,'2011-04-21 22:47:48','2011-04-24 18:07:04'),
-	(7,'Learn the language of the ghetto.','2011-04-28 15:50:00',0,'2011-04-21 22:47:48','0000-00-00 00:00:00'),
-	(8,'Rewrite Sesame Street to feature more cats and less spelling.','2011-04-29 23:07:00',0,'2011-04-21 22:47:48','0000-00-00 00:00:00'),
-	(9,'Consume own body weight in red liquorice.','2011-05-30 10:45:00',0,'2011-04-21 22:47:48','0000-00-00 00:00:00'),
-	(10,'Finish writing this bloody test data','2011-05-28 14:35:00',0,'2011-04-21 22:47:48','2011-04-24 21:59:18'),
-	(11,'weweIm so bored of writing this now','2011-05-29 09:12:00',1,'2011-04-21 22:47:48','2011-04-24 21:59:37'),
-	(12,'How many more of these do I have to do?','2011-06-03 11:00:00',0,'2011-04-21 22:47:48','0000-00-00 00:00:00'),
-	(13,'Oh, 2 more.','2011-06-05 16:30:00',0,'2011-04-21 22:47:48','0000-00-00 00:00:00'),
-	(17,'nice new todo','2011-04-24 07:30:00',0,'2011-04-25 11:30:03','0000-00-00 00:00:00');
+	(6,'fix this broken add todo issue','2011-05-08 16:00:00',0,'2011-05-02 21:59:41','2011-05-02 22:14:52',1),
+	(12,'feed chilli plants','2011-05-03 00:00:00',0,'2011-05-02 22:31:01','2011-05-02 22:31:01',1),
+	(13,'New years day','2012-01-01 00:00:00',0,'2011-05-02 22:31:27','2011-05-02 22:31:27',1),
+	(14,'The day after tomorrow','2006-05-11 00:00:00',0,'2011-05-02 22:32:25','2011-05-02 22:40:32',1),
+	(15,'The day after tomorrow','2011-04-05 00:00:00',0,'2011-05-02 22:36:19','2011-05-02 22:36:19',1),
+	(16,'My birthday','2011-05-15 00:00:00',0,'2011-05-02 22:41:44','2011-05-02 22:42:00',1),
+	(17,'Vandas birthday','2017-05-11 00:00:00',0,'2011-05-02 22:42:27','2011-05-02 22:42:27',1),
+	(18,'Halloween!','2031-10-11 00:00:00',0,'2011-05-02 22:44:32','2011-05-02 22:44:32',1),
+	(19,'Guy Fawkes Night','2011-11-05 00:00:00',0,'2011-05-02 22:45:20','2011-05-02 22:45:20',1),
+	(20,'whats this one going to do?','2011-12-10 00:00:00',0,'2011-05-02 22:49:12','2011-05-02 22:49:12',1),
+	(21,'25th of May',NULL,0,'2011-05-02 22:50:10','2011-05-02 22:50:10',1);
 
 /*!40000 ALTER TABLE `todos` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -110,8 +119,17 @@ CREATE TABLE `user_tokens` (
   UNIQUE KEY `uniq_token` (`token`),
   KEY `fk_user_id` (`user_id`),
   CONSTRAINT `user_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
+LOCK TABLES `user_tokens` WRITE;
+/*!40000 ALTER TABLE `user_tokens` DISABLE KEYS */;
+INSERT INTO `user_tokens` (`id`,`user_id`,`user_agent`,`token`,`created`,`expires`)
+VALUES
+	(6,1,'cb9487f4f3bf5d51dbf790949121a6ada9d3dd50','rLoZzjr1I3nFghzG66s4EjafEhzgtetj',1304345864,1305555464),
+	(7,1,'40d20dcf1e3cc3c57da6ff1acfcf23aaf33fd7b9','eEoMMOyhFjo4s7561LQPiFdWamJRacEn',1304346071,1305555671);
+
+/*!40000 ALTER TABLE `user_tokens` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table users
@@ -129,8 +147,16 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `uniq_username` (`username`),
   UNIQUE KEY `uniq_email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`,`email`,`username`,`password`,`logins`,`last_login`)
+VALUES
+	(1,'terry.morgan76@gmail.com','shodan_uk','8f36e48fc0e046b687718a8e9292a25e4f246ce9b5037b375e',17,1304369767);
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 
